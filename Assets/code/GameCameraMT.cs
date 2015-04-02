40410b424e2266b7b3eb8a13343e47715bf897ea
arclee <arclee0117@gmail.com>
2015-04-02 16:36:47 +0800

    cam mt

--- Assets/code/GameCameraMT.cs
+++ Assets/code/GameCameraMT.cs
@@ -10,14 +10,20 @@ public class GameCameraMT : MonoBehaviour {
 		ByOther
 	}
 
-	public List<GameObject> targets = new List<GameObject>();
-	public GameObject target;
+	public class CamData
+	{
+
+	}
+
+	//鏡頭起始位置.
+	public GameObject defaultRefPosObj;
+	Vector3 refDeltapos;
+
+	public GameObject currentTarget;
 	public GameObject target1;
 	public GameObject target2;
-	public GameObject targetArvg;
-	public GameObject defaultRefPosObj;	
-	Vector3 refDeltapos;
-	//Vector3 lastPlayPos;
+	public GameObject targetArvg;//多 target 時的平均位置點.
+
 	Quaternion startRotate;
 	public CamPlayMode camPlayMode = CamPlayMode.My;
 
@@ -37,14 +43,12 @@ public class GameCameraMT : MonoBehaviour {
 	}
 
     public bool isDelayCam = true;
-    Vector3 startOffset = new Vector3();
     public float MoveFactor = 5.0f;
-    bool isFirstRunUpdate = true;
 
 	//
-	float olddist = -1.0f;
-	float oldOthsize = 5.0f;
-	public float oldOthsizeprefix = 1.2f;
+	float olddist = 0.0f;
+	float oldOthsize = 0.0f;
+	public float othSizeAdd = 0.0f;
 
 	public void SwitchPlayMode(CamPlayMode pm)
 	{
@@ -69,61 +73,84 @@ public class GameCameraMT : MonoBehaviour {
 
 	void Start ()
 	{
-		if (target == null)
+		oldOthsize = Camera.main.orthographicSize;
+		startRotate = transform.rotation;
+
+		//算出偏移.
+		if (defaultRefPosObj != null)
+		{
+			refDeltapos = transform.position - defaultRefPosObj.transform.position;
+		}
+
+		//建出多 target 時的平均位置點.
+		if (targetArvg == null)
+		{
+			targetArvg = new GameObject("targetArvg");
+			targetArvg.transform.parent = gameObject.transform;
+		}
+
+		//至少要有一個 tartet.
+		if (target1 == null)
 		{
 			return;
 		}
+
+		CaluteCam();
+
+	}
+
+	public void CaluteCam()
+	{
+		currentTarget = target1;
 		
 		if (target1 != null && target2 != null)
 		{
 			targetArvg.transform.position = Vector3.Lerp(target1.transform.position, target2.transform.position, 0.5f);
-			target = targetArvg;
+			currentTarget = targetArvg;
 			float dist = Vector3.Distance(target1.transform.position, target2.transform.position);
 			olddist = dist;
-			oldOthsize = Camera.main.orthographicSize;
 		}
 
-		lastpos = target.transform.position;
-		startRotate = transform.rotation;
 	}
 
-	public void setTarget(GameObject go)
+
+	public void setTarget1(GameObject go)
 	{
-		target = go;
-
-		if (defaultRefPosObj != null)
-		{
-			refDeltapos = transform.position - defaultRefPosObj.transform.position;
-
-			transform.position = refDeltapos;
-		}
-
-
-		lastpos = target.transform.position;
-
+		target1 = go;
+		CaluteCam();
+	}
+	public void setTarget2(GameObject go)
+	{
+		target2 = go;
+		CaluteCam();
 	}
 
 	// Update is called once per frame
 	void LateUpdate ()
 	{
-		if (target == null)
+		if (currentTarget == null)
 		{
 			return;
 		}
 		
 		if (target1 != null && target2 != null)
 		{
+			//中間的位置.
 			targetArvg.transform.position = Vector3.Lerp(target1.transform.position, target2.transform.position, 0.5f);
 			float dist = Vector3.Distance(target1.transform.position, target2.transform.position);
-			float scl = dist/olddist;
-			Camera.main.orthographicSize = oldOthsize + (scl - oldOthsizeprefix);
+
+			//防止為0.
+			if (dist == 0)
+			{
+				Camera.main.orthographicSize = oldOthsize;
+			}
+			else
+			{
+				float o = (dist * oldOthsize) / olddist;
+				Camera.main.orthographicSize = o + othSizeAdd;
+			}
 		}
 
-        if (isFirstRunUpdate)
-        {
-            isFirstRunUpdate = false;
-			startOffset = target.transform.position - transform.position;
-        }
 		
 		switch(camPlayMode)
 		{
@@ -133,23 +160,19 @@ public class GameCameraMT : MonoBehaviour {
 			{   
 				if (isDelayCam)
 				{
-					Vector3 MoveToPos = target.transform.position - startOffset;
-					transform.position = Vector3.Lerp(transform.position, MoveToPos, Time.deltaTime * MoveFactor);
-			
+					Vector3 MoveToPos = currentTarget.transform.position + refDeltapos;
+					transform.position = Vector3.Lerp(transform.position, MoveToPos, Time.deltaTime * MoveFactor);			
 
 				}
 				else
 				{
-					Vector3 pos = target.transform.position - lastpos;
-					lastpos = target.transform.position;
-					transform.Translate(pos, Space.World);
-				}
-				
+					transform.position = currentTarget.transform.position + refDeltapos;
+				}				
 			}
 			
 			if (lookat)
 			{
-				transform.LookAt(target.transform);
+				transform.LookAt(currentTarget.transform);
 			}
 			break;
 		}
@@ -164,6 +187,5 @@ public class GameCameraMT : MonoBehaviour {
 	
 	void OnPostRender()
 	{
-		//PoseInputScript.DebugDraw();
 	}
 }
